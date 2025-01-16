b2eb935ee4790c6defe5b2e40cffa9688120bfe9
Favour Peters <favourpeters1@gmail.com>
2025-01-16 12:14:07 +0100

    nutrition form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import What from "./pages/What";
 import Privacy from "./pages/privacy";
+import Nutrition from "./pages/NutritionForm";
 import "./App.css";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/what-we-offer" element={<What />} />
       <Route path="/privacy-policy" element={<Privacy />} />
+      <Route path="/nutritionform" element={<Nutrition />} />
     </Routes>
   );
 };
